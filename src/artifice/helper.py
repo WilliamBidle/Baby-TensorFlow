8da4726a9e528bfc0291d21594b2e411da2a420c
William Bidle <wbidle5@gmail.com>
2024-04-19 15:28:18 -0400

    updating helper.py

--- src/artifice/helper.py
+++ src/artifice/helper.py
@@ -42,14 +42,8 @@ def one_hot_encode(labels):
 
     num_unique_elements = np.unique(labels)
 
-    dic = {}
-    counter = 0
-    for i in num_unique_elements:
-        if i in dic:
-            pass
-        else:
-            dic[i] = counter
-            counter += 1
+    # Create a dictionary mapping unique elements to their index
+    dic = {val: idx for idx, val in enumerate(num_unique_elements)}
 
     encoded_labels = np.zeros((len(labels), len(num_unique_elements)))
 
@@ -57,25 +51,3 @@ def one_hot_encode(labels):
         encoded_labels[index][dic[label]] = 1.0
 
     return encoded_labels
-
-
-def load_model(filename="Saved_Model"):
-    """
-    Loads a model.
-
-    :params filename:
-    """
-
-    load_path = "Saved Models/" + filename
-
-    with open(load_path, "rb") as fp:  # Unpickling
-        loaded = pickle.load(fp)
-
-    weights = loaded[0]
-    activations = loaded[1]
-
-    nn = NN()  # initialize a blank Neural Network
-    nn.weights = weights
-    nn.activation_funcs = activations
-
-    return nn
